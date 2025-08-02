c7266e82d73e85345b10059472963177c7130d92
Pheamph <149634707+timmy1524@users.noreply.github.com>
2025-08-02 16:45:49 -0700

    fix tiny UI errors

--- components/ui/AuthForms/Signup.tsx
+++ components/ui/AuthForms/Signup.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Button from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import Input from '@/components/ui/Input';
 import React from 'react';
 import Link from 'next/link';
