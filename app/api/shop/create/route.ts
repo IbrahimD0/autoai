ef3eb3ba3162da2c4be6f900e01f1e9c31559354
Sandeep Salwan <salwansandeep5@gmail.com>
2025-08-02 19:28:24 -0700

    init

--- app/api/shop/create/route.ts
+++ app/api/shop/create/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: NextRequest) {
       .from('shops')
       .select('*')
       .eq('user_id', user.id)
-      .single();
+      .maybeSingle();
 
     let shop: Shop;
 
